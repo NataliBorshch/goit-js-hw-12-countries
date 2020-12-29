af663c7c1d92330af84abef009ef2b3f9122d44e
NataliBorshch <67202688+NataliBorshch@users.noreply.github.com>
2020-12-29 19:19:15 +0200

    test-2

--- src/index.js
+++ src/index.js
@@ -3,39 +3,29 @@ import { refs } from './js/refs';
 import { fetchCountries } from './js/fetchCountries';
 import listTemplates from './templates/list-country.hbs';
 import countryTemplates from './templates/country.hbs';
+import debounce from 'lodash.debounce';
 
 import { alert } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 const _ = require('lodash');
 
-refs.inputRefs.addEventListener('input', _.debounce(foundCountry, 500));
+refs.inputRefs.addEventListener('input', debounce(foundCountry, 500));
 
 function foundCountry(event) {
   const name = event.target.value;
   clear();
-  fetchCountries(name)
-    .then(data => {
-      const length = data.length;
-      const oneCountry = countryTemplates(data);
-      const listCountry = listTemplates(data);
-      const message =
-        "<p class ='text-not-found'>Sorry, but you  Request  incorrect </p>";
-
-      if (length > 1 && length <= 10) {
-        const ul = `<ul>${listCountry}</ul>`;
-        createMarkup(ul);
-      } else if (length === 1) {
-        createMarkup(oneCountry);
-      } else if (length > 10) {
-        alert({
-          text: 'Too many matches found.Please entry a more specific  query!',
-        });
-      } else {
-        createMarkup(message);
-      }
-    })
-    .catch(console.error);
+  if (!name) {
+    return;
+  } else {
+    fetchCountries(name)
+      .then(data => {
+        const length = data.length;
+        responseRate(length, data);
+      })
+      .catch(console.error);
+  }
 }
 
 function createMarkup(items) {
@@ -43,4 +33,23 @@ function createMarkup(items) {
 }
 function clear() {
   return (refs.boxCountry.innerHTML = '');
+}
+
+function responseRate(count, data) {
+  if (count > 1 && count <= 10) {
+    const listCountry = listTemplates(data);
+    const ul = `<ul>${listCountry}</ul>`;
+    createMarkup(ul);
+  } else if (count === 1) {
+    const oneCountry = countryTemplates(data);
+    createMarkup(oneCountry);
+  } else if (count > 10) {
+    alert({
+      text: 'Too many matches found.Please entry a more specific  query!',
+    });
+  } else {
+    const message =
+      "<p class ='text-not-found'>Sorry, but you  Request  incorrect </p>";
+    createMarkup(message);
+  }
 }
